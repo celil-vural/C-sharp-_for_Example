6dcb20880c44db28dc9df6934a182f7a28262acc
celil-vural <celilvural.work@gmail.com>
2020-05-25 14:52:19 +0300

    github c sharp proje

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_hesapla = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lbl_default = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_asalSayi = new System.Windows.Forms.Button();
+            this.btn_mukemmelSayi = new System.Windows.Forms.Button();
+            this.btn_mutuSayi = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(118, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MUTLU SAYI MI?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(63, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sayıyı giriniz:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btn_hesapla
+            // 
+            this.btn_hesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_hesapla.Location = new System.Drawing.Point(143, 163);
+            this.btn_hesapla.Name = "btn_hesapla";
+            this.btn_hesapla.Size = new System.Drawing.Size(123, 35);
+            this.btn_hesapla.TabIndex = 3;
+            this.btn_hesapla.Text = "Hesapla";
+            this.btn_hesapla.UseVisualStyleBackColor = true;
+            this.btn_hesapla.Click += new System.EventHandler(this.btn_hesapla_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(629, 370);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox3.Controls.Add(this.lbl_default);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.btn_hesapla);
+            this.groupBox3.Location = new System.Drawing.Point(199, 37);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(394, 327);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            // 
+            // lbl_default
+            // 
+            this.lbl_default.AutoSize = true;
+            this.lbl_default.Location = new System.Drawing.Point(18, 282);
+            this.lbl_default.Name = "lbl_default";
+            this.lbl_default.Size = new System.Drawing.Size(32, 13);
+            this.lbl_default.TabIndex = 4;
+            this.lbl_default.Text = "mutlu";
+            this.lbl_default.Visible = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox2.Controls.Add(this.btn_asalSayi);
+            this.groupBox2.Controls.Add(this.btn_mukemmelSayi);
+            this.groupBox2.Controls.Add(this.btn_mutuSayi);
+            this.groupBox2.Location = new System.Drawing.Point(7, 37);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(175, 327);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // btn_asalSayi
+            // 
+            this.btn_asalSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_asalSayi.Location = new System.Drawing.Point(17, 206);
+            this.btn_asalSayi.Name = "btn_asalSayi";
+            this.btn_asalSayi.Size = new System.Drawing.Size(140, 50);
+            this.btn_asalSayi.TabIndex = 2;
+            this.btn_asalSayi.Text = "Asal Sayi";
+            this.btn_asalSayi.UseVisualStyleBackColor = true;
+            this.btn_asalSayi.Click += new System.EventHandler(this.btn_asalSayi_Click);
+            // 
+            // btn_mukemmelSayi
+            // 
+            this.btn_mukemmelSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_mukemmelSayi.Location = new System.Drawing.Point(17, 117);
+            this.btn_mukemmelSayi.Name = "btn_mukemmelSayi";
+            this.btn_mukemmelSayi.Size = new System.Drawing.Size(140, 44);
+            this.btn_mukemmelSayi.TabIndex = 1;
+            this.btn_mukemmelSayi.Text = "Mükemmel Sayi";
+            this.btn_mukemmelSayi.UseVisualStyleBackColor = true;
+            this.btn_mukemmelSayi.Click += new System.EventHandler(this.btn_mukemmelSayi_Click);
+            // 
+            // btn_mutuSayi
+            // 
+            this.btn_mutuSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_mutuSayi.Location = new System.Drawing.Point(17, 20);
+            this.btn_mutuSayi.Name = "btn_mutuSayi";
+            this.btn_mutuSayi.Size = new System.Drawing.Size(140, 44);
+            this.btn_mutuSayi.TabIndex = 0;
+            this.btn_mutuSayi.Text = "Mutlu Sayı";
+            this.btn_mutuSayi.UseVisualStyleBackColor = true;
+            this.btn_mutuSayi.Click += new System.EventHandler(this.btn_mutuSayi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 406);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_hesapla;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_asalSayi;
+        private System.Windows.Forms.Button btn_mukemmelSayi;
+        private System.Windows.Forms.Button btn_mutuSayi;
+        private System.Windows.Forms.Label lbl_default;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_mutuSayi_Click(object sender, EventArgs e)
+        {
+            lbl_default.Text = "mutlu";
+            label1.Text="Mutlu sayı mı?";
+        }
+
+        private void btn_mukemmelSayi_Click(object sender, EventArgs e)
+        {
+            lbl_default.Text = "mukemmel";
+            label1.Text="Mükemmel sayı mı?";
+        }
+
+        private void btn_asalSayi_Click(object sender, EventArgs e)
+        {
+            lbl_default.Text = "asal";
+            label1.Text="Asal sayı mı?";
+        }
+        static int numSquareSum(int n)
+        {
+            int squareSum = 0;
+            while (n != 0)
+            {
+                squareSum += (n % 10) *
+                             (n % 10);
+                n /= 10;
+            }
+            return squareSum;
+        }
+
+        static bool isHappynumber(int n)
+        {
+            int slow, fast;
+            slow = fast = n;
+            do
+            {
+                slow = numSquareSum(slow);
+                fast = numSquareSum(numSquareSum(fast));
+            }
+            while (slow != fast);
+            return (slow == 1);
+        }
+        static bool perfectNumber(int num)
+        {
+            int n=num,toplam=0;
+            List<int> liste=new List<int>();
+            for (int i=1;i<num;i++)
+            {
+                if ((num%i)==0)
+                {
+                    liste.Add(i);
+                }
+            }
+            foreach(int list in liste)
+            {
+                toplam+=list;
+            }
+            if (toplam == num)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void btn_hesapla_Click(object sender, EventArgs e)
+        {
+            if (lbl_default.Text == "mutlu")
+            {
+                int n = int.Parse(textBox1.Text);
+                if (isHappynumber(n))
+                {
+                    MessageBox.Show(n +
+                    " sayısı mutlu sayı");
+                }
+
+                else
+                {
+                    MessageBox.Show(n +
+                    " sayısı mutlu sayı değil");
+                }
+            }
+            else if (lbl_default.Text == "mukemmel")
+            {
+                int num=int.Parse(textBox1.Text);
+                if (perfectNumber(num))
+                {
+                    MessageBox.Show(num+ " sayısı mükemmel sayıdır.");
+                }
+                else
+                {
+                    MessageBox.Show(num+" sayısı mükemmel sayı değildir.");
+                }
+            }
+            else if(lbl_default.Text=="asal")
+            {
+                bool asalmi=true;
+                if (int.Parse(textBox1.Text) <= 2)
+                {
+                    MessageBox.Show("1 ve 2 sayıları asaldır.\n 1 den küçük sayılarda asal işlemi yapılmaz...");
+                }
+                else
+                {
+                    for (int i = 2; i < int.Parse(textBox1.Text); i++)
+                    {
+                        if (int.Parse(textBox1.Text) % i == 0)
+                        {
+                            asalmi=false;
+                        }
+                    }
+                    if(asalmi==false) MessageBox.Show("sayı asal değil");
+                    else MessageBox.Show("sayı asal");
+                }
+            }
+        }
+    }
+}
